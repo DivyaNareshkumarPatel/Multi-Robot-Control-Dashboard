ed05061143917c05d8c47aa4cbf9a423ccf2c3f6
DivyaNareshkumarPatel <divyanareshkumarpatel@gmail.com>
2025-03-12 14:23:08 +0530

    onlinr server connected

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = 'http://localhost:5000' || 'https://multi-robot-control-dashboard.onrender.com';
+const url =  'https://multi-robot-control-dashboard.onrender.com' || 'http://localhost:5000';
 
 export const login = async ({ email, password }) => {
     try {
